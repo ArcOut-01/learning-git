e17e4e8cc5cb29fae16dafb85beefe579a10bf57
Marcin Tokarczyk <tokarczykmarcin.dev@gmail.com>
2024-01-24 00:58:16 +0100

    Modified 'print_word.py' to print lemonade 5 times

--- print_word.py
+++ print_word.py
@@ -1,4 +1,4 @@
 print("This is vesion of the program in which word 'lemonade' "
-      + "is printed 3 times")
-for _ in range(3):
+      + "is printed 5 times")
+for _ in range(5):
     print('lemonade')
